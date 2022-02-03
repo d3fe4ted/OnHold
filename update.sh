47a9c76bba9aa27f6aa6d6535ac3249b6c7c4f1e
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-02-02 18:44:01 -0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+rm -rf client.c
+rm -rf server.py
+git clone https://github.com/d3fe4ted/OnHold/blob/main/client.c
+git clone https://github.com/d3fe4ted/OnHold/blob/main/server.py
+python2 server.py
