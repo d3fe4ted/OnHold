293c55e4505b7f94852280f2774d2caeff3cacc2
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-04-14 16:31:31 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
+sudo apt install wget
+sudo yum install wget
+sudo
 rm -rf client.c
 rm -rf server.py
-git clone https://github.com/d3fe4ted/OnHold/blob/main/client.c
-git clone https://github.com/d3fe4ted/OnHold/blob/main/server.py
+wget https://raw.githubusercontent.com/d3fe4ted/OnHold/main/client.c
+wget https://raw.githubusercontent.com/d3fe4ted/OnHold/main/server.py
 python2 server.py
